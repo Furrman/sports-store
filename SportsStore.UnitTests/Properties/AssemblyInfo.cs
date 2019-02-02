7bbd53bd01a7386232d6e7700d0f44cc767f58fe
Furrman <furmanek.hubert@gmail.com>
2019-02-02 18:38:59 +0000

    Initial push to GitHub

---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd8459dc-262d-48e5-a723-aeff8c9e6090")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.HtmlHelpers;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CanPaginate()
+        {
+            // Assign
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductId = 1, Name = "P1"},
+                new Product {ProductId = 2, Name = "P2"},
+                new Product {ProductId = 3, Name = "P3"},
+                new Product {ProductId = 4, Name = "P4"},
+                new Product {ProductId = 5, Name = "P5"},
+            });
+
+            var controller = new ProductController(mock.Object) {PageSize = 3};
+
+            // Act
+            var result = (ProductsListViewModel) controller.List(null, 2).Model;
+
+            // Assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual("P4", prodArray[0].Name);
+            Assert.AreEqual("P5", prodArray[1].Name);
+        }
+
+        [TestMethod]
+        public void CanGeneratePageLinks()
+        {
+            // Assign
+            HtmlHelper myHelper = null;
+            var pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+            Func<int, string> pageUrlDelegate = i => "Strona" + i;
+
+            // Act
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            // Assert
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Strona1"">1</a>"
+                            + @"<a class=""btn btn-default btn-primary selected"" href=""Strona2"">2</a>"
+                            + @"<a class=""btn btn-default"" href=""Strona3"">3</a>",
+                result.ToString());
+        }
+
+        [TestMethod]
+        public void CanSendPaginationViewModel()
+        {
+            // Assign
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductId = 1, Name = "P1"},
+                new Product {ProductId = 2, Name = "P2"},
+                new Product {ProductId = 3, Name = "P3"},
+                new Product {ProductId = 4, Name = "P4"},
+                new Product {ProductId = 5, Name = "P5"},
+            });
+
+            var controller = new ProductController(mock.Object) { PageSize = 3 };
+
+            // Act
+            var result = (ProductsListViewModel)controller.List(null, 2).Model;
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(2, pageInfo.CurrentPage);
+            Assert.AreEqual(3, pageInfo.ItemsPerPage);
+            Assert.AreEqual(5, pageInfo.TotalItems);
+            Assert.AreEqual(2, pageInfo.TotalPages);
+        }
+
+        [TestMethod]
+        public void CanFilterProducts()
+        {
+            // Assign
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductId = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductId = 2, Name = "P2", Category = "Cat2"},
+                new Product {ProductId = 3, Name = "P3", Category = "Cat1"},
+                new Product {ProductId = 4, Name = "P4", Category = "Cat2"},
+                new Product {ProductId = 5, Name = "P5", Category = "Cat3"},
+            });
+
+            var controller = new ProductController(mock.Object) { PageSize = 3 };
+
+            // Act
+            Product[] result = ((ProductsListViewModel)controller.List("Cat2", 1).Model).Products.ToArray();
+
+            // Assert
+            Assert.AreEqual(2, result.Length);
+            Assert.IsTrue(result[0].Name == "P2");
+            Assert.IsTrue(result[0].Category == "Cat2");
+            Assert.IsTrue(result[1].Name == "P4");
+            Assert.IsTrue(result[1].Category == "Cat2");
+        }
+    }
+}
